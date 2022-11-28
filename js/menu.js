731e44104f2ec68e5750e5be8d8c53f752e40188
Iryna Khamidova <waterholein@gmail.com>
2022-11-28 14:53:17 +0200

    +

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,17 @@ const refs = {
 	openMenuBtn: document.querySelector("[data-open-menu]"),
 	closeMenuBtn: document.querySelector("[data-close-menu]"),
 	menu: document.querySelector("[data-menu]"),
+	menuList: document.querySelector('menu-nav'),
 };
 
 refs.openMenuBtn.addEventListener("click", toggleMenu);
 refs.closeMenuBtn.addEventListener("click", toggleMenu);
+refs.menuList.addEventListener("click", removeMenu);
 
 function toggleMenu() {
 	refs.menu.classList.toggle("is-hidden");
+}
+function removeMenu() {
+	refs.menu.classList.add("is-hidden");
+	refs.body.classList.remove("no-scroll");
 }
